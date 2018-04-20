af2927b1eb19ddd77a89399f02c8df7e0c505491
DanielGeek <daniel30081990@gmail.com>
2018-04-20 11:01:05 -0400

    Agregado recursos frontend

--- frontend/vistas/plantilla.php
+++ frontend/vistas/plantilla.php
@@ -1,12 +1,33 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Sistema Administrativo</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="title" content="Tienda Virtual">
+	<meta name="description" content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut, consequuntur? Repudiandae delectus maxime ex ullam tempora veniam facilis aliquam necessitatibus ducimus velit quos, fugiat numquam nobis sit accusantium atque quis?">
+	<meta name="keyword" content="Lorem ipsum | dolor | sit | amet consectetur adipisicing elit. Aut, consequuntur? Repudiandae delectus maxime ex ullam tempora veniam facilis aliquam necessitatibus ducimus velit quos, fugiat numquam nobis sit accusantium atque quis?">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Tienda Virtual</title>
+
+	<base href="vistas/">
+	<link rel="stylesheet" href="css/plugins/bootstrap.min.css">
+	<link rel="stylesheet" href="css/plugins/font-awesome.min.css">
+	
+	<script src="js/plugins/jquery.min.js"></script>
+	<script src="js/plugins/bootstrap.min.js"></script>
+	
+
+
 </head>
 <body>
-    <h1>Titulo</h1>
+
+<?php
+
+// Cabezote
+
+include "modulos/cabezote.php";
+
+?>
+
 </body>
 </html>
